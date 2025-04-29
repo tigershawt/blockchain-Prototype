26db33831bdfa896e483085a62675602d18be8aa
Dmitriy <dimitriystarkov@gmail.com>
2025-04-29 13:53:56 +0200

    pukpuk

--- app.py
+++ app.py
@@ -238,4 +238,4 @@ def api_asset_history(asset_id):
 if __name__ == '__main__':
     # Starting the server
     logger.info("Starting InLock Blockchain API Server")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
